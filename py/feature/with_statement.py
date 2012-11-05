86495dfd4645b5a77aa1660884f5cbdd765ae626
yxy <yan5yang@gmail.com>
2012-11-05 14:55:15 +0800

    add python feature

---
+++ py/feature/with_statement.py
@@ -0,0 +1,12 @@
+"""
+class ControlledExecuteion:
+    def __enter__(self):
+        set things up
+        return thins
+
+    def __exit(self, type, value, traceback):
+        tear things down
+
+with ControlledExecuteion() as things:
+    some code
+"""
